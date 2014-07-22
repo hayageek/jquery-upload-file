57ac4f3a06ec54f34511cb78eb89f3b3ce62538b
Ravishanker Kusuma <rskusuma@gmail.com>
2014-07-22 16:03:57 +0530

    Version bump

--- js/jquery.uploadfile.js
+++ js/jquery.uploadfile.js
@@ -1,6 +1,6 @@
 /*!
  * jQuery Upload File Plugin
- * version: 3.1.9
+ * version: 3.1.10
  * @requires jQuery v1.5 or later & form plugin
  * Copyright (c) 2013 Ravishanker Kusuma
  * http://hayageek.com/
