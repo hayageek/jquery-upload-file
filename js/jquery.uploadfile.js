fcc5c9a11bb91e7242c27e34e5c7d74cb110ce27
Ravishanker Kusuma <ravishanker@dbs.com>
2015-09-07 19:25:44 +0800

    Fixed issues

--- js/jquery.uploadfile.js
+++ js/jquery.uploadfile.js
@@ -1,6 +1,6 @@
 /*!
  * jQuery Upload File Plugin
- * version: 4.0.2
+ * version: 4.0.4
  * @requires jQuery v1.5 or later & form plugin
  * Copyright (c) 2013 Ravishanker Kusuma
  * http://hayageek.com/
